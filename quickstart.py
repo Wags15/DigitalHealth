2de53a4a3ab37fd6bcaa429a2220f240b9958c84
Wags15 <danwags15@gmail.com>
2022-03-13 06:35:14 -0400

    finished product?
    if it doesnt work delete token.json and try again

--- quickstart.py
+++ quickstart.py
@@ -40,21 +40,52 @@ async def createEvents():
     tot_hrs = 0
     events = []
     while flag:
+        clone = False
         name = str(input("Event Name: "))
         if(name.lower() == 'exit'):
             flag= False
             break
         length = int(input("How Long Do You Expect This Task To Complete (Minutes): "))
+        if(length >= 90):
+            length = length/2
+            clone = True
         length = int(math.ceil(length/15) * 15)
         # if(tot_hrs + length >= avail_hrs):
         #     print("Not Enough Time In The Day!")
         #     break
         # tot_hrs += length
         difficulty = int(input("How Hard Is This Task (1-easy 2-medium 3-hard): "))
+        if(clone == True):
+            events.append(Event(name,length,difficulty))
         events.append(Event(name,length,difficulty))
-    await fillCal(events)
+    await eventQueue(events)
+   #  await fillCal(events)
 
     
+async def eventQueue(events):
+    """Flips between hardest then easiest then second hardest, second easiest, etc.
+    """
+    new_order = []
+    events.sort(key=lambda x: x.difficulty, reverse=True)
+
+    s_break = Event("Break", 15, 0)
+    m_break = Event("Break", 30, 0)
+
+    while len(events) > 0:
+        new_order.append(events[0])
+        if(events[0].length >= 60):
+            new_order.append(m_break)
+        else:
+            new_order.append(s_break)
+        del events[0]
+        if(len(events)>0):
+            new_order.append(events[-1])
+            if(events[-1].length >= 60):
+                new_order.append(m_break)
+            else:
+                new_order.append(s_break)
+            del events[-1]
+    await fillCal(new_order)
 
 def workingHours():
     flag = True
@@ -91,42 +122,71 @@ def myRound(x):
 
 async def getPrevEvents():
     calendar = [False] * 96
+    # global end
+    # global start
+    # creds = None
+    # # The file token.json stores the user's access and refresh tokens, and is
+    # # created automatically when the authorization flow completes for the first
+    # # time.
+    # if os.path.exists('token.json'):
+    #     creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # # If there are no (valid) credentials available, let the user log in.
+    # if not creds or not creds.valid:
+    #     if creds and creds.expired and creds.refresh_token:
+    #         creds.refresh(Request())
+    #     else:
+    #         flow = InstalledAppFlow.from_client_secrets_file(
+    #             'credentials.json', SCOPES)
+    #         creds = flow.run_local_server(port=0)
+    #     # Save the credentials for the next run
+    #     with open('token.json', 'w') as token:
+    #         token.write(creds.to_json())
 
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-
-    try:
-        service = build('calendar', 'v3', credentials=creds)     
-        events_result = service.events().list(calendarId='primary', timeMax = end.isoformat()+"Z", timeMin=start.isoformat()+"Z",
-                                              maxResults=10, singleEvents=True,
-                                              orderBy='startTime').execute()
-        events = events_result.get('items', [])  
-        print(len(events))
+    # try:
+    #     service = build('calendar', 'v3', credentials=creds)     
+    #     events_result = service.events().list(calendarId='primary', timeMax = end.isoformat()+"Z", timeMin=start.isoformat()+"Z",
+    #                                           maxResults=10, singleEvents=True,
+    #                                           orderBy='startTime').execute()
+    #     events = events_result.get('items', [])  
 
      
 
-    except HttpError as error:
-        print('An error occurred: %s' % error)
+    # except HttpError as error:
+    #     print('An error occurred: %s' % error)
+
+    # for event in events:
+    #     start = event['start'].get('dateTime', event['start'].get('date'))
+    #     end = event['end'].get('dateTime', event['end'].get('date'))
+    #     print(start)
+    #     print(end)
+    global start
+    global end
+    start_in_min = ((start.hour * 60) + start.minute)/15
+    print(start_in_min)
+    end_in_min = ((end.hour * 60) + end.minute)/15
+    print(end_in_min)
+
+
+    i=0
+    with open('./cal.txt','r') as file:
+        lines = file.readlines()
+    
+    for line in lines:
+        if(line != "True\n"):
+            calendar[i] = False
+        else:
+            calendar[i] = True
+
+        if(i <= start_in_min or i >= end_in_min):
+            calendar[i] = True
+        
+        i+=1
 
     return calendar
 
 
 async def fillCal(events):
+    calendar = [False] * 96
     calendar = await getPrevEvents()
     # for event in events:
     #     start_index = event['start'].get('dateTime')
@@ -137,26 +197,34 @@ async def fillCal(events):
         for i in range(len(calendar)):
             if(calendar[i] is False and i+required_slots <= len(calendar)):
                 fits = True
+                         
                 for j in range(i, i+required_slots):    
-                    print(j)               
                     if calendar[j] == True:
                         fits = False
                         
                 if fits:
-                    print(event.name, " FITS")
                     for j in range(i, i+required_slots):
                         calendar[j] = True
                     event.start = datetime.datetime.utcnow()
                     start_hour = math.floor((i*15)/ 60)
-                    start_min = (i*15) - start_hour*60
+                    start_min = (i*15) - start_hour*60    
                     event.start = event.start.replace(hour=start_hour, minute=start_min,second=0, microsecond=0)
-
                     event.end = datetime.datetime.utcnow()
                     end_hour = math.floor((i+required_slots) * 15 /60)
                     end_min = ((i+required_slots)*15) - end_hour*60
                     event.end = event.end.replace(hour=end_hour, minute=end_min, second=0,microsecond=0)
+    
+                    
                     await addEvent(event)
+                    
                     break
+    f = open("./cal.txt", "w")
+    f.write('')
+    f.close()
+    for x in calendar:
+        with open("./cal.txt", 'a') as cal:
+            cal.write(str(x)+'\n')
+
     
 async def addEvent(event):
     date = datetime.datetime.now()
@@ -185,12 +253,21 @@ async def addEvent(event):
             'description': 'A chance to hear more about Google\'s developer products.',
             'start': {
                 'dateTime': ""+str(event.start.isoformat())+"",
-                'timeZone': 'America/Los_Angeles',
+                'timeZone': 'GMT-4:00',
             },
             'end': {
                 'dateTime': ""+str(event.end.isoformat())+"",
-                'timeZone': 'America/Los_Angeles',
-            },                            
+                'timeZone': 'GMT-4:00',
+            },   
+            "reminders": {
+                "useDefault": 'false',
+                "overrides": [
+                    {
+                        "method":'popup',
+                        "minutes": 1
+                    }
+                ]
+            },                     
         }
 
         new_event = service.events().insert(calendarId='primary', body=new_event).execute()
@@ -215,7 +292,7 @@ class Event():
     def __init__(self, name, length, difficulty):
         self.name = name
         self.length = length
-        self.start = datetime.datetime.now()
+        self.start = datetime.datetime.utcnow()
         self.end = self.start
         self.difficulty = difficulty
         self.next = None
